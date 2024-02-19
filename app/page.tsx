4d179f0e1bd048aa9f9cdfbc6c51e512b03cb705
NaotoCoding <naoto.zaniah@gmail.com>
2024-02-19 20:00:43 +0900

    地図とgeotiffをleafletで表示

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,29 @@
-const Home = () => <></>
+'use client'
+
+import { MapContainer, TileLayer } from "react-leaflet"
+import GeotiffLayer from "./_components/leaflet/child_components/GeotiffLayer"
+import 'leaflet/dist/leaflet.css'
+
+const Home = () => {
+  const geotiffPath = 'geo.tif'
+  const mapAttribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  const mapUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+
+  return(
+    <div style={{height: '1000px', width: '1500px'}}>
+      <MapContainer style={{ height: '70%', width: '70%'}}
+                    center={[0,0]}
+                    zoom={9}
+                    scrollWheelZoom={false}
+      >
+        <GeotiffLayer geotiffPath={geotiffPath} />
+        <TileLayer
+          attribution={mapAttribution}
+          url={mapUrl}
+        />
+    </MapContainer>
+    </div>
+  )
+}
 
 export default Home
